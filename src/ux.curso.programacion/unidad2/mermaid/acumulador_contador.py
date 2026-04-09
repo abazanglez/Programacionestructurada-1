adc897338676b12eca758e7ab9de9a41d4007a4f
abazanglez <abazanglez@gmail.com>
2026-04-08 22:27:58 -0600

    ejercicio02

---
+++ src/ux.curso.programacion/unidad2/mermaid/acumulador_contador.py
@@ -0,0 +1,28 @@
+def acumulacion():
+    
+    total_acumulado = 0
+    semanas = 0
+    meta = 2500
+
+   
+    while total_acumulado < meta:
+       
+        salario_semanal = float(input("Ingresa el salario de la semana: "))
+        
+        
+        total_acumulado = total_acumulado + salario_semanal
+        semanas = semanas + 1
+
+    
+    return semanas
+
+
+def main():
+    resultado = acumulacion()
+    
+   
+    print("Semanas trabajadas:", resultado)
+
+
+if __name__ == "__main__":
+    main()
